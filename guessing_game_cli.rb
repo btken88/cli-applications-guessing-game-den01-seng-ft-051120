e47e7bbf0d561c1ddd44d25e68ea81a45c726734
Bryce Kennedy <btken88@gmail.com>
2020-05-02 18:13:07 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -5,7 +5,7 @@ def run_guessing_game
   if input == comp_number
     puts "You guessed the correct number!"
   elsif input == 'exit'
-    puts "Goodbye"
+    puts "Goodbye!"
   else
     puts "Sorry! The computer guessed #{comp_number}"
   end
